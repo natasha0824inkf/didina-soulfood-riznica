a5d11ffee7e22a4a6ac3a67a921b2a0d0b845b8a
natasha0824inkf <natasha0824inkf@users.noreply.github.com>
2026-09-12 17:12:56 +0000

    deploy: 98cd60d38422893fefaba82054719cc22dfcea0f

--- js/recipes-data.js
+++ js/recipes-data.js
@@ -2157,6 +2157,6 @@ const recipes = [
     note: "Jednostavno, šareno i savršeno za laganu večeru. 😋",
     image: "assets/images/peceni-slatki-krompir-sa-fetom.jpg",
     imgPos: "center 45%",
-    category: "Recepti koji mirišu iz rerne"
+    category: "Recepti kada ne znam šta da kuvam"
   }
 ];
